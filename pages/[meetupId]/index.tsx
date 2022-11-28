f7f7778863c09f9c3135cedcbce2311958bc6274
Luigi Matias <igiboi26@gmail.com>
2022-11-28 11:19:32 -0500

    nextjs meetup project

---
+++ pages/[meetupId]/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div><h1>Meet up ID</h1></div>
+  )
+}
+
+export default index
---
+++ pages/new-meetup/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div><h1>New Meet Up Page</h1></div>
+  )
+}
+
+export default index
